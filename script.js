dc3ee4cf53f02161915c0de359ef3e19e087cf09
lucas borges <67933184+imlucvs@users.noreply.github.com>
2023-04-16 20:20:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sideopen() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("sidebtn").style.display = "none";
+  }
+  
+  function sideclose() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("sidebtn").style.display = "block";
+  }
